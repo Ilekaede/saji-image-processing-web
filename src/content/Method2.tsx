9ab19199d5b262831e7433530be2e2d340acaf5b
Ilekaede <asakuramotiyo@gmail.com>
2024-08-14 01:07:51 +0900

    fix: 説明ページをmdファイル->tsxファイルに変更し，それに対応させた

---
+++ src/content/Method2.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Box, Text } from "@chakra-ui/react";
+const Method1 = () => {
+  return (
+    <Box p={4}>
+      <Text as="h1" fontSize="3xl" fontWeight="bold" fontFamily="Arial">
+        特徴点抽出
+      </Text>
+      <Text fontSize="xl" fontFamily="Verdana">
+        特徴点抽出は,
+        画像の中から重要なポイントや特徴的な部分を抽出する技術です.
+      </Text>
+    </Box>
+  );
+};
+
+export default Method1;
