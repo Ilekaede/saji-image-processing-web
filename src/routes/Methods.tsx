25a8c30f46dc5aebf3ac0f715ef6ddd0289eefd9
Ilekaede <asakuramotiyo@gmail.com>
2024-08-13 15:04:20 +0900

    画像処理手法ページにブログの記事カードを実装した

--- src/routes/Methods.tsx
+++ src/routes/Methods.tsx
@@ -1,27 +1,61 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Heading,
+  Text,
+  Stack,
+  Image,
+  Button,
+} from "@chakra-ui/react";
+import sajiRogo from "../icon/saji_rogo.png";
+
+const articles = [
+  { id: 1, title: "2値化" },
+  { id: 2, title: "特徴点抽出" },
+];
 
 const Methods = () => {
   return (
     <div>
-      <h1>Methods</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="binarization">Binarization</Link>
+      <ul>
+        {articles.map((article) => (
+          <li key={article.id}>
+            <Card
+              direction={{ base: "column", sm: "row" }}
+              overflow="hidden"
+              variant="outline"
+              marginBottom="20px"
+            >
+              <Image
+                objectFit="cover"
+                maxW={{ base: "100%", sm: "200px" }}
+                src="https://bit.ly/dan-abramov"
+                alt="free-image"
+              />
+
+              <Stack>
+                <CardBody>
+                  <Heading size="md">{article.title}</Heading>
+
+                  <Text py="2">2値化をより応用的に学んでみましょう</Text>
+                </CardBody>
+
+                <CardFooter>
+                  <Link to={`/methods/${article.id}`}>
+                    <Button variant="solid" colorScheme="blue">
+                      View More
+                    </Button>
+                  </Link>
+                </CardFooter>
+              </Stack>
+            </Card>
           </li>
-          <li>
-            <Link to="geometric_transformation">Geometric Transformation</Link>
-          </li>
-        </ul>
-      </nav>
-      {/* <Routes>
-        <Route path="binarization" element={<Binarization />} />
-        <Route
-          path="geometric_transformation"
-          element={<GeometricTransformation />}
-        />
-      </Routes> */}
+        ))}
+      </ul>
     </div>
   );
 };
